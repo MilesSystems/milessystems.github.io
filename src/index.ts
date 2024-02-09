7342519081f0310466fd543d89865ddd13046081
richardtmiles <richard@miles.systems>
2024-02-09 09:32:04 -0700

    adding back fontawesome

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import "/node_modules/bootstrap/dist/css/bootstrap.min.css"
 import "/node_modules/admin-lte/dist/css/AdminLTE.min.css"
 import "/node_modules/admin-lte/dist/css/skins/skin-red-light.min.css"
 
-
 import * as jQuery from "jquery"
+
+import "/node_modules/font-awesome/css/font-awesome.css"
 
 // @ts-ignore
 window.$ = window.jQuery = jQuery
