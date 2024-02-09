7a209c2faa4492a6d479d2330f3aa3f2b4dd3664
richardtmiles <richard@miles.systems>
2024-02-09 09:26:06 -0700

    updating repo to use a compiler

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import "/node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "/node_modules/admin-lte/dist/css/AdminLTE.min.css"
+import "/node_modules/admin-lte/dist/css/skins/skin-red-light.min.css"
+
+
+import * as jQuery from "jquery"
+
+// @ts-ignore
+window.$ = window.jQuery = jQuery
+
+$(()=>{
+
+    require("/node_modules/bootstrap/dist/js/bootstrap.min.js")
+
+})
+
