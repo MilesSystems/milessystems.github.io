3e50eca4e5fd2f664e87fb8c2c7512032398a53a
richardtmiles <richard@miles.systems>
2024-02-13 16:06:25 -0700

    updating home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,102 +1,133 @@
 export default function home() {
 
-    return <div className="col-md-offset-1 col-md-10">
-        <div id="pjax-content">
-            <section className="invoice">
-                <div className="row">
-                    <div className="col-xs-12">
-                        <h2 className="page-header">
-                            <i className="fa fa-globe"></i> <a href="https://miles.systems/">Miles Systems LLC</a>
-                            <small className="pull-right">01/22/2024</small>
-                        </h2>
-                    </div>
-                </div>
-                <div className="row invoice-info">
-                    <div className="col-sm-4 invoice-col">
-                        <address>Email:
-                            <strong>RichardMiles@Stats.Coach</strong><br/>
-                        </address>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-xs-12 table-responsive">
-                        <div className="box-body">
-                            <h1>We are committed to student success, developing leaders, <br/>
-                                and encouraging lifelong wellness.</h1>
+    return <>
+        <div className="col-md-offset-1 col-md-10">
+            <div id="pjax-content">
+                <script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js"></script>
+                <div className="col-xs-12 col-sm-offset-3 col-sm-9 col-md-offset-5 col-md-7"
+                     style={{marginTop: "50px", marginBottom: "100px"}}>
+                    <section className="top-card-layout top-card-layout--show-current-hide-past-position">
+                        <figure className="cover-img onload lazy-loaded" data-delayed-background=""
+                                style={{backgroundImage: "url('/assets/img/Carbon-black.png')"}}></figure>
+                        <div className="top-card-layout__card">
+                            <div className="top-card__profile-image-container" data-section="picture"><img
+                                className="artdeco-entity-image artdeco-entity-image--profile artdeco-entity-image--circle-8 top-card-layout__entity-image top-card__profile-image top-card__profile-image--real-image onload lazy-loaded"
+                                data-ghost-classes="artdeco-entity-image--ghost artdeco-entity-image--ghost-profile"
+                                data-ghost-url="https://static-exp1.licdn.com/sc/h/djzv59yelk5urv2ujlazfyvrk"
+                                alt="Richard Tyler Miles" src="/assets/img/defaults/richard2.png"/><img
+                                className="artdeco-entity-image artdeco-entity-image--profile artdeco-entity-image--circle-8 artdeco-entity-image--ghost artdeco-entity-image--ghost-profile top-card-layout__entity-image top-card__profile-image top-card__profile-image--ghost-image onload lazy-loaded"
+                                data-ghost-classes="artdeco-entity-image--ghost artdeco-entity-image--ghost-profile"
+                                data-ghost-url="https://static-exp1.licdn.com/sc/h/djzv59yelk5urv2ujlazfyvrk"
+                                alt="Richard Tyler Miles"
+                                src="https://static-exp1.licdn.com/sc/h/djzv59yelk5urv2ujlazfyvrk"/></div>
+                            <div className="top-card-layout__entity-info-container">
+                                <div className="top-card-layout__entity-info"><h1
+                                    className="top-card-layout__title">Richard
+                                    Tyler
+                                    Miles</h1>
+                                    <h2 className="top-card-layout__headline">Sr. Software Developer &amp; Founder</h2>
+                                    <h3 className="top-card-layout__first-subline"><span
+                                        className="top-card__subline-item">Highlands Ranch, Colorado</span>
+                                    </h3>
+                                    <div className="top-card-layout__cta-container">
+                                        <a
+                                            className="top-card-layout__cta top-card-layout__cta--primary"
+                                            href="https://www.linkedin.com/signup/public-profile-join?vieweeVanityName=richardtmiles&amp;trk=public_profile-settings_top-card-primary-button-join-to-connect"
+                                            data-tracking-control-name="public_profile-settings_top-card-primary-button-join-to-connect"
+                                            data-tracking-will-navigate="">Message on LinkedIn <i
+                                            className="fa fa-linkedin-square" aria-hidden="true"
+                                            style={{paddingLeft: "5px"}}></i>
+                                        </a></div>
+                                </div>
+                                <div
+                                    className="top-card-layout__entity-info top-card-layout__entity-info--right-column">
+                                    <div className="top-card__links-container">
+                                        <div data-section="currentPositionsDetails"
+                                             className="top-card__right-column-link top-card__position-info with-transition">
+                                            <a className="top-card-link top-card-link--link"
+                                               href="https://www.linkedin.com/company/mckesson?trk=public_profile-settings_topcard_current_company"
+                                               data-tracking-will-navigate=""
+                                               data-tracking-control-name="public_profile-settings_topcard_current_company"><img
+                                                className="artdeco-entity-image artdeco-entity-image--company artdeco-entity-image--square-1 top-card-link__logo lazy-loaded"
+                                                data-ghost-classes="artdeco-entity-image--ghost artdeco-entity-image--ghost-company"
+                                                data-ghost-url="https://static-exp1.licdn.com/sc/h/ck48xrmh3ctwna0w2y1hos0ln"
+                                                alt="McKesson" src="/assets/img/work/dropingaming.jpeg"/><span
+                                                className="top-card-link__description">Drop-in Gaming</span></a>
+                                        </div>
 
-                            <h2>Stats Coach </h2> ("us", "we", or "our") operates https://Stats.Coach (the "Site"). This
-                            page informs you of our policies regarding the
-                            collection, use and disclosure of Personal Information we receive from users of the Site.
-                            We use your Personal Information only for providing and improving the Site. By using the
-                            Site, you agree to the collection and use of
-                            information in accordance with this policy.
 
-                            <h3>Information Collection And Use</h3>
-                            While using our Site, we may ask you to provide us with certain personally identifiable
-                            information that can be used to contact or identify you.
-                            Personally identifiable information may include, but is not limited to your name ("Personal
-                            Information").
+                                        <div data-section="pastPositionsDetails"
+                                             className="top-card__right-column-link top-card__position-info with-transition">
+                                            <a className="top-card-link top-card-link--link"
+                                               href="https://www.linkedin.com/company/mckesson?trk=public_profile-settings_topcard_current_company"
+                                               data-tracking-will-navigate=""
+                                               data-tracking-control-name="public_profile-settings_topcard_past_company"><img
+                                                className="artdeco-entity-image artdeco-entity-image--company artdeco-entity-image--square-1 top-card-link__logo lazy-loaded"
+                                                data-ghost-classes="artdeco-entity-image--ghost artdeco-entity-image--ghost-company"
+                                                data-ghost-url="https://static-exp1.licdn.com/sc/h/ck48xrmh3ctwna0w2y1hos0ln"
+                                                alt="McKesson" src="/assets/img/work/mckesson.jpeg"/><span
+                                                className="top-card-link__description">McKesson</span></a></div>
 
-                            <h3>Log Data</h3>
-                            Like many site operators, we collect information that your browser sends whenever you visit
-                            our Site ("Log Data").
-                            This Log Data may include information such as your computer's Internet Protocol ("IP")
-                            address, browser type, browser version, the pages of our
-                            Site that you visit, the time and date of your visit, the time spent on those pages and
-                            other statistics.
-                            In addition, we may use third party services such as Google Analytics that collect, monitor
-                            and analyze this ...
 
-                            <h3>Communications</h3>
-                            We may use your Personal Information to contact you with newsletters, marketing or
-                            promotional materials and other information that ...
-                            <br/>
-                            The Log Data section is for businesses that use analytics or tracking services in
-                            websites or apps, like Google Analytics. For the full
-                            disclosure section, create your own Privacy Policy.
-                            <br/>
-                            The Communications section is for businesses that may contact users via email (email
-                            newsletters) or other methods. For the full disclosure
-                            section, create your own Privacy Policy.
+                                        <div data-section="pastPositionsDetails"
+                                             className="top-card__right-column-link top-card__position-info with-transition">
+                                            <a className="top-card-link top-card-link--link"
+                                               href="https://www.linkedin.com/company/praesidium-inc-?trk=public_profile-settings_topcard_past_company"
+                                               data-tracking-will-navigate=""
+                                               data-tracking-control-name="public_profile-settings_topcard_past_company"><img
+                                                className="artdeco-entity-image artdeco-entity-image--company artdeco-entity-image--square-1 top-card-link__logo lazy-loaded"
+                                                data-ghost-classes="artdeco-entity-image--ghost artdeco-entity-image--ghost-company"
+                                                data-ghost-url="https://static-exp1.licdn.com/sc/h/ck48xrmh3ctwna0w2y1hos0ln"
+                                                alt="Praesidium, Inc."
+                                                src="/assets/img/work/praesidium.jpeg"/><span
+                                                className="top-card-link__description">Praesidium, Inc.</span></a>
+                                        </div>
 
-                            <h3>Cookies</h3>
-                            Cookies are files with small amount of data, which may include an anonymous unique
-                            identifier. Cookies are sent to your browser from a web site
-                            and stored on your computer's hard drive.
-                            Like many sites, we use "cookies" to collect information. You can instruct your
-                            browser to refuse all cookies or to indicate when a cookie is
-                            being sent. However, if you do not accept cookies, you may not be able to use some
-                            portions of our Site.
 
-                            <h3>Security</h3>
-                            The security of your Personal Information is important to us, but remember that no
-                            method of transmission over the Internet, or method of
-                            electronic storage, is 100% secure. While we strive to use commercially acceptable
-                            means to protect your Personal Information, we cannot
-                            guarantee its absolute security.
+                                        <div data-section="educationsDetails"
+                                             className="top-card__right-column-link top-card__education-info with-transition">
+                                            <a className="top-card-link top-card-link--link"
+                                               href="https://www.linkedin.com/school/northtexas/?trk=public_profile-settings_topcard_school"
+                                               data-tracking-will-navigate=""
+                                               data-tracking-control-name="public_profile-settings_topcard_school"><img
+                                                className="artdeco-entity-image artdeco-entity-image--school artdeco-entity-image--square-1 top-card-link__logo lazy-loaded"
+                                                data-ghost-classes="artdeco-entity-image--ghost artdeco-entity-image--ghost-school"
+                                                data-ghost-url="https://static-exp1.licdn.com/sc/h/8zyvax2j09k9jfspjn2wi17un"
+                                                alt="University of North Texas"
+                                                src="/assets/img/work/northtexas.png"/><span
+                                                className="top-card-link__description">University of North Texas</span></a>
+                                        </div>
 
-                            <h3>Changes To This Privacy Policy</h3>
 
-                            This Privacy Policy is effective as of 5/1/2015 and will remain in effect except
-                            with respect to any changes in its provisions in the future,
-                            which will be in effect immediately after being posted on this page.
-                            We reserve the right to update or change our Privacy Policy at any time and you
-                            should check this Privacy Policy periodically. Your continued
-                            use of the Service after we post any modifications to the Privacy Policy on this
-                            page will constitute your acknowledgment of the modifications
-                            and your consent to abide and be bound by the modified Privacy Policy.
-                            If we make any material changes to this Privacy Policy, we will notify you either
-                            through the email address you have provided us, or by placing
-                            a prominent notice on our website.
-
-                            <h3>Contact Us</h3>
-                            If you have any questions about this Privacy Policy, please contact us.
-
+                                        <a href="/Portfolio">
+                                            <div data-section="websites"
+                                                 className="top-card__right-column-link top-card__websites with-transition">
+                                                <div>
+                                                    <button className="modal__outlet websites__outlet"
+                                                            data-tracking-control-name="public_profile-settings_websites-modal-outlet"
+                                                            data-modal="default-outlet">
+                                                        <div className="top-card-link">
+                                                            <img
+                                                                className="top-card-link__logo top-card-link__logo--link-icon artdeco-entity-image artdeco-entity-image--square-1 lazy-loaded"
+                                                                alt="Websites" aria-hidden="true"
+                                                                src="https://static-exp1.licdn.com/sc/h/9o8qqup6da04vhqijz8ft1j5g"/>
+                                                            <span
+                                                                className="top-card-link__description">Portfolio</span>
+                                                        </div>
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
+                    </section>
                 </div>
 
-            </section>
+                <div className="clearfix"></div>
+            </div>
         </div>
-    </div>
+        <div className="clearfix"></div>
+    </>
 }
